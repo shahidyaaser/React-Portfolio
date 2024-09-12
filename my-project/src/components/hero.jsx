fd67a9a24f2397842733f19dec2d98583c5a581d
shahidyaaser <shahidyaaser18@gmail.com>
2024-09-12 21:58:01 +0530

    URL changed

--- my-project/src/components/hero.jsx
+++ my-project/src/components/hero.jsx
@@ -45,7 +45,7 @@ function Hero() {
                             initial="hidden"
                             animate="visible"
                             variants={container(1.5)}
-                            href="https://drive.google.com/drive/folders/16mJwK2x19H1ULaM7gdgEVR0eSg0ioMrK"
+                            href="https://drive.google.com/file/d/12oLe3T1oHUZnJtCXoAlq-goxg3VuyTj0/view?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="mt-4 inline-block px-6 py-3 bg-purple-600 text-white font-medium text-lg rounded-full shadow-lg hover:bg-purple-700 transition-colors duration-300">
