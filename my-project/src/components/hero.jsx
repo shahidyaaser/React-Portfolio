2f04ffd6759bab9c918f90f3bb1c51fb70e6227d
Shahid yaaser <42862587+shahidyaaser@users.noreply.github.com>
2024-09-16 21:26:50 +0530

    Update hero.jsx

--- my-project/src/components/hero.jsx
+++ my-project/src/components/hero.jsx
@@ -71,7 +71,7 @@ function Hero() {
                 <div className='w-full lg:w-1/2 lg:p-8'>
                     <div className='flex justify-center'>
                         <motion.img
-                            className="w-[500px] h-[500px] rounded-lg object-scale-down"
+                            className="w-[500px] h-[600px] rounded-lg object-cover"
                             initial={{ x: 100, opacity: 0 }}
                             animate={{ x: 0, opacity: 1 }}
                             transition={{ duration: 1, delay: 1.2 }}
