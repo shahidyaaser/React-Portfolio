dd6ef4263be939b37a7a21ff5697d0e8998d831e
Shahid yaaser <42862587+shahidyaaser@users.noreply.github.com>
2024-09-16 21:18:45 +0530

    Update navbar.jsx

--- my-project/src/components/navbar.jsx
+++ my-project/src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from "../assets/kevinRushLogo.png";
+// import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 function Navbar() {
     return (
         <nav className='mb-20 flex items-center justify-between py-6'>
-            <div className='flex flex-shrink-0 items-center'>
+{/*             <div className='flex flex-shrink-0 items-center'>
                 <img className='mx-2 w-10' src={logo} alt="Shahid's logo" />
-            </div>
+            </div> */}
             <div className='m-8 flex justify-center items-center gap-4 text-2xl'>
                 <a href="https://www.linkedin.com/in/shahid-yaaser-6066671b9/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin />
